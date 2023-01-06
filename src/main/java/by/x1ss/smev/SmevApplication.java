c2a409b9f1a82cebd28caf084022bd5503441abe
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-06 15:31:42 +0300

    init commit

---
+++ src/main/java/by/x1ss/smev/SmevApplication.java
@@ -0,0 +1,13 @@
+package by.x1ss.smev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/x1ss/smev/SmevApplicationTests.java
@@ -0,0 +1,13 @@
+package by.x1ss.smev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
