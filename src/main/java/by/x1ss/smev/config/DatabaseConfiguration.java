f91759d992d6998f777568240a991ecbd0f7e85a
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-28 22:11:18 +0300

    delete dataSource and jdbcTemplate creation bean

--- src/main/java/by/x1ss/smev/config/DatabaseConfiguration.java
+++ src/main/java/by/x1ss/smev/config/DatabaseConfiguration.java
@@ -1,31 +1,8 @@
 package by.x1ss.smev.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
 
-    @Value("${spring.datasource.username}")
-    private String dbUsername;
-
-    @Value("${spring.datasource.password}")
-    private String dbPassword;
-
-    @Bean
-    public DataSource dataSource() {
-        return new DriverManagerDataSource(dbUrl, dbUsername, dbPassword);
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
 }
